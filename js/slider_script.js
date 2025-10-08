30e090952289ed16c81c32ea52ebc87c2776db2f
Unity <Unity@rbiu.local>
2025-10-08 09:30:07 +0500

    slider2

--- js/slider_script.js
+++ js/slider_script.js
@@ -19,3 +19,25 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-scrollbar',
   },
 });
+
+const otherProductsSlider = new Swiper('.other-products-slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+  // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
