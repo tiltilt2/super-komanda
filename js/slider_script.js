1691298ea4a921b72e69f252341f0acdc6bfd1c5
Unity <Unity@rbiu.local>
2025-10-15 09:15:54 +0500

    в каталог добавила слайдер

--- js/slider_script.js
+++ js/slider_script.js
@@ -1,43 +1,47 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: false,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    nextEl: ".custom-next-btn",
+    prevEl: ".custom-prev-btn",
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
+
+  loop: true, // бесконечная прокрутка
 });
 
-const otherProductsSlider = new Swiper('.other-products-slider', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
+const otherProductsSlider = new Swiper(".other-products-slider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
   // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
