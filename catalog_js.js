d1dd6c5f2df532e4b123b2e050a87408d6c71c4b
yukasik <ykass894@inbox.ru>
2025-11-04 17:08:58 +0500

    сделала лайк и поиск по товарам

--- catalog_js.js
+++ catalog_js.js
@@ -1,18 +1,18 @@
-function searchProducts() {
+// поиск через кнопку "лупа" и enter
+function searchProducts(event) {
+  event.preventDefault();
+
   const searchInput = document.getElementById("searchInput");
-  const cardsContainer = document.getElementById("cardsContainer");
-  const cards = cardsContainer.querySelectorAll(".card");
+  const query = searchInput.value.trim().toLowerCase();
+  const cards = document.querySelectorAll(".card");
+  const noResultsMessage = document.getElementById("noResultsMessage");
 
-  const noResults =
-    document.getElementById("noResultsMessage") ||
-    createNoResults(cardsContainer);
-
-  const searchText = searchInput.value.toLowerCase().trim();
   let found = false;
 
   cards.forEach((card) => {
     const title = card.querySelector(".card-title").textContent.toLowerCase();
-    if (title.includes(searchText)) {
+
+    if (title.includes(query) || query === "") {
       card.style.display = "block";
       found = true;
     } else {
@@ -20,16 +20,28 @@ function searchProducts() {
     }
   });
 
-  noResults.style.display = found ? "none" : "block";
+  if (!found) {
+    noResultsMessage.style.display = "block";
+  } else {
+    noResultsMessage.style.display = "none";
+  }
 }
 
-function createNoResults(container) {
-  const noResults = document.createElement("p");
-  noResults.id = "noResultsMessage";
-  noResults.textContent = "Ничего не найдено";
-  noResults.style.textAlign = "center";
-  noResults.style.fontWeight = "500";
-  noResults.style.display = "none";
-  container.appendChild(noResults);
-  return noResults;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const searchForm = document.querySelector(".search");
+
+  searchForm.addEventListener("submit", (event) => {
+    searchProducts(event);
+  });
+});
+
+// кнопка избранного
+document.addEventListener("DOMContentLoaded", () => {
+  const favoriteButtons = document.querySelectorAll(".favorite-btn");
+
+  favoriteButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("active");
+    });
+  });
+});
